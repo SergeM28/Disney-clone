56fcf908eaeb40c909435549280a2b5bab583ff7
serge.muradian@gmail.com <serge.muradian@gmail.com>
2023-11-02 15:24:51 +0300

    Make responsive navigation bar

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import logo from '../assets/Images/logo.png'
 import { HiHome, HiMagnifyingGlass, HiStar, HiPlayCircle, HiTv } from 'react-icons/hi2'
 import { HiPlus, HiDotsVertical } from 'react-icons/hi'
 import HeaderItem from './HeaderItem'
 
 const Header = () => {
+    const [toggle, setToggle] = useState(false);
     const menu = [
         {
             name: 'HOME',
@@ -35,14 +36,28 @@ const Header = () => {
         <div className="flex items-center justify-between p-5">
             <div className="flex items-center gap-8">
                 <img src={logo} className="w-[80px] md:w-[115px] object-cover" />
-                {menu.map((item) => (
-                    <div>
+                <div className="hidden lg:flex gap-8">
+                    {menu.map((item) => (
                         <HeaderItem name={item.name} Icon={item.icon} />
+                    ))}
+                </div>
+                <div className="flex lg:hidden gap-5">
+                    {menu.map((item, index) => index < 3 && (
+                        <HeaderItem name={''} Icon={item.icon} />
+                    ))}
+                    <div className="lg:hidden" onClick={() => setToggle(!toggle)}>
+                        <HeaderItem name={''} Icon={HiDotsVertical} />
+                        {toggle ? <div className="absolute mt-3 bg-[#121212] border-[1px]
+                        p-3 border-gray-700 px-5 py-4">
+                            {menu.map((item, index) => index > 2 && (
+                                <HeaderItem name={item.name} Icon={item.icon} />
+                            ))}
+                        </div> : null }
                     </div>
-                ))}
+                </div>
             </div>
             <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745"
-            className="w-[40px] rounded-full"/>
+                className="w-[40px] rounded-full" />
         </div>
     )
 }
